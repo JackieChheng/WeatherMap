bef8c50fa38f3d866dec7dafe3d84c532db628dc
Jackie Chheng <jackiechheng@Jackies-MacBook-Pro.local>
2023-10-25 15:18:32 -0600

    added javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
-const searchButton = document.querySelector(".search.btn")
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.querySelector(".search-btn");
+    const cityInput = document.querySelector(".city-input");
+    const apiKey = '96c9264a940f91bc89a76d3fcd83e67c'; 
+    const cnt = 5; 
+
+    const updateWeatherData = (cityName, lat, lon) => {
+        const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${cnt}&appid=${apiKey}`;
+
+        fetch(weatherAPI)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+
+                data.list.filter(forecast => {})
+            })
+            .catch(() => {
+                alert('An error has occurred getting weather forecast');
+            });
+    }
+
+    const fetchWeatherData = () => {
+        const cityName = cityInput.value.trim();
+        if (!cityName) return;
+
+        const geoAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+
+        fetch(geoAPI)
+            .then((res) => res.json())
+            .then((data) => {
+                if (!data.name) {
+                    return alert(`Coordinates could not be found for ${cityName}`);
+                }
+                const { name, coord } = data;
+                const { lat, lon } = coord;
+                updateWeatherData(name, lat, lon);
+            })
+            .catch(() => {
+                alert('An error has occurred getting coordinates');
+            });
+    }
+
+    searchButton.addEventListener("click", fetchWeatherData);
+});
